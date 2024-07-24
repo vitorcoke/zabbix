4534e13e5cc5e4a519acde7fffa8c7b2c84fc765
Vitor Coke <cokevitor@gmail.com>
2024-07-24 19:54:37 -0300

    corrigido

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
+import { VerifyRamaisModule } from './schedules/ramais/verify-ramais.module';
 import { Module } from '@nestjs/common';
-import { RamaisModule } from './schedules/ramais/ramais.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AsteriskModule } from './app/asterisk/asterisk.module';
 import { ZabbixModule } from './app/zabbix/zabbix.module';
 
 @Module({
   imports: [
-    RamaisModule,
+    VerifyRamaisModule,
     ScheduleModule.forRoot(),
     AsteriskModule,
     ZabbixModule,
